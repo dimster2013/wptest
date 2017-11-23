d08ebd16030ec6e8d58633cabaf7be134e0e3d41
dimitric <couwenberg@gmail.com>
2017-11-24 10:59:08 +1100

    done

--- src/Scroll.js
+++ src/Scroll.js
@@ -9,17 +9,13 @@ export default class Scroll extends React.Component {
     }
 
     onScroll = event => {
-
         const {scrollStep, names, displayItems} = this.props;
-
         if (event.nativeEvent.wheelDelta > 0) {
-
             this.setState({
                 position: Math.max(
                     this.state.position - scrollStep, 0)
             });
         } else {
-
             this.setState({
                 position: Math.min(
                     this.state.position + scrollStep, names.length - displayItems)
@@ -28,7 +24,6 @@ export default class Scroll extends React.Component {
     };
 
     render() {
-
         let displayedNames = this.props.names.slice(
             this.state.position,
             this.state.position + this.props.displayItems
